e211fe7548ddf5d54d68db3da44583370d481fa2
hi2sudha <159509240+hi2sudha@users.noreply.github.com>
2024-02-09 00:19:41 -0500

    Add files via upload
    Week 1 Day 1 Assignments 03-02-2024

---
+++ FindNumber.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class FindNumber {
+	public static void main(String[] args) {
+
+	int Number=60;
+	
+	if(Number>0){
+		System.out.println("The Number is positive:"+Number);
+			}
+	else{
+			  System.out.println("The Number is negative:"+Number);
+			}
+
+	}
+	
+}
+	
+
+
---
+++ Firefox.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float browserVersion=120.02F;
+		String browserName ="Edge";
+		boolean pageLoaded = true;
+		int noOfBrowser=1;
+		char browserLogo='E';
+		System.out.println("Browser Version:"+browserVersion);
+		System.out.println("Browser Name: "+browserName+"\n"+"Page Loaded: "+pageLoaded+"\n"+"No of Browser: "+noOfBrowser+"\n"+"Browser Logo: "+browserLogo);
+
+	}
+
+}
